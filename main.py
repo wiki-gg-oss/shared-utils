04611b25ccbbdb9f12ba043f82151a2964b7eb5e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-05-02 18:10:39 -0500

    some fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+import requests.exceptions
 from mwcleric import AuthCredentials
 from mwcleric import WikiggClient
 from mwclient import AssertUserFailedError
@@ -7,18 +8,20 @@ from mwclient.page import Page
 
 
 class Loadout:
+    target_name = 'thedeepabyss'
     startat_namespace = 0
     startat_page = None
     # noinspection PyRedeclaration
-    # startat_page = 'Module:Navbox/Aether II/en'  # uncomment & edit this line if you want to resume running in the middle
+    # startat_page = 'MediaWiki:Wikigg-socialmedia-jointext-sidebar'  # uncomment & edit this line if you want to resume running in the middle
+
     overwrite_existing = True
     summary = 'Adding default set of pages'
 
     def __init__(self):
         self.passed_startat = False
-        credentials = AuthCredentials(user_file="me", use_site_pw=False)  # set to True iff the wiki is onboarding
+        credentials = AuthCredentials(user_file="me", use_site_pw=True)  # set to True iff the wiki is onboarding
         self.loadout = WikiggClient('defaultloadout')
-        self.target = WikiggClient('gg', credentials=credentials)  # edit the wiki here
+        self.target = WikiggClient(self.target_name, credentials=credentials)  # edit the wiki here
 
     def run(self):
         self.copy()
@@ -30,7 +33,7 @@ class Loadout:
             if ns > self.startat_namespace - 1:  # ns 4 is Project ns
                 for orig_page in self.loadout.client.allpages(namespace=ns):
                     orig_page: Page
-                    time.sleep(1)
+                    time.sleep(.75)
                     print(orig_page.name)
                     new_title = orig_page.name
                     if ns == 4:
@@ -42,20 +45,30 @@ class Loadout:
                     target_page = self.target.client.pages[new_title]
                     if self.overwrite_existing or not target_page.exists:
                         try:
-                            target_page.save(orig_page.text(), summary=self.summary)
-                            protections = '|'.join([f'{k}={v[0]}' for k, v in orig_page.protection.items()])
-                            self.target.protect(target_page, protections=protections)
+                            self.save(target_page, orig_page)
+                        except requests.exceptions.HTTPError:
+                            time.sleep(20)
+                            self.save(target_page, orig_page)
 
-                        except AssertUserFailedError:
-                            self.target.login()
-                            target_page.save(orig_page.text(), summary=self.summary)
+    def save(self, target_page: Page, orig_page: Page):
+        self.target.save(target_page, text=orig_page.text(), summary=self.summary)
+        protections = '|'.join([f'{k}={v[0]}' for k, v in orig_page.protection.items()])
+        if protections != '':
+            self.target.protect(target_page, protections=protections)
 
     def move(self):
-        loadout_mainpage = self.loadout.client.site['mainpage']
-        target_mainpage = self.target.client.site['mainpage']
-        self.target.client.pages[loadout_mainpage].move(target_mainpage, reason="Move main page for new wiki",
-                                                        no_redirect=True)
-        self.target.client.pages['Main Page'].save(f'#redirect [[{target_mainpage}]]')
+        if self.overwrite_existing is False:
+            return
+        loadout_mainpage_name = self.loadout.client.site['mainpage']
+        target_mainpage_name = self.target.client.site['mainpage']
+
+        target_new_mainpage = self.target.client.pages[loadout_mainpage_name]
+        target_orig_mainpage = self.target.client.pages[target_mainpage_name]
+        self.target.delete(target_orig_mainpage, "To move default main page")
+        self.target.move(target_new_mainpage, target_mainpage_name, reason="Move main page for new wiki",
+                         no_redirect=True, ignore_warnings=True)
+        if target_mainpage_name != "Main Page":
+            self.target.client.pages['Main Page'].save(f'#redirect [[{target_mainpage_name}]]')
 
 
 if __name__ == '__main__':
