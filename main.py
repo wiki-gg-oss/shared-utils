f77e29281353a27c520f8016421897dab6f948ac
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-05-09 09:03:40 -0500

    fix main page

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ class Loadout:
 
     def run(self):
         self.copy()
-        self.move()
 
     def copy(self):
         for ns in self.loadout.client.namespaces:
@@ -55,33 +54,21 @@ class Loadout:
         new_title = orig_page.name
         if ns == 4:
             new_title = f'Project:{orig_page.page_title}'
+        if orig_page.name == self.loadout.client.site['mainpage']:
+            new_title = self.target.client.site['sitename']
         target_page = self.target.client.pages[new_title]
         if self.overwrite_existing or not target_page.exists:
             self.save(target_page, orig_page)
 
     def save(self, target_page: Page, orig_page: Page):
-        self.target.save(target_page, text=orig_page.text(), summary=self.summary)
+        text = orig_page.text()
+        if target_page.name == 'Main Page':
+            target_mainpage_name = self.target.client.site['sitename']
+            text = f'#redirect [[{target_mainpage_name}]]'
+        self.target.save(target_page, text, summary=self.summary)
         protections = '|'.join([f'{k}={v[0]}' for k, v in orig_page.protection.items()])
         if protections != '':
             self.target.protect(target_page, protections=protections)
-
-    def move(self):
-        # TODO: handle this immediately when creating the pages
-        if self.overwrite_existing is False:
-            return
-        loadout_mainpage_name = self.loadout.client.site['mainpage']
-        target_mainpage_name = self.target.client.site['mainpage']
-        if target_mainpage_name == 'Main Page':
-            # support not overwriting the main page message if it was previously existing
-            # but if it was "Main Page" then force the new location because siteinfo is cached
-            # TODO: force cache update here
-            target_mainpage_name = self.target.client.site['sitename']
-
-        target_mainpage = self.target.client.pages[loadout_mainpage_name]
-        self.target.move(target_mainpage, target_mainpage_name, reason="Move main page for new wiki",
-                         no_redirect=True, ignore_warnings=True)
-        if target_mainpage_name != "Main Page":
-            self.target.client.pages['Main Page'].save(f'#redirect [[{target_mainpage_name}]]')
 
 
 if __name__ == '__main__':
