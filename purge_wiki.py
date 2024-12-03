1430a30d6a0cde102ceb3f7c0262f37f5f441dfa
alex4401 <rylatgl@gmail.com>
2024-12-03 22:13:26 +0100

    Fancy rewrite of purge_wiki with threading, namespace filtering, OPTIONSSSS
    - `-w`/`--warmup` to warm up HTML cache within your nearest CF DC.
    - `-e`/`--nulledit` to do a null-edit.
    - `-t 4`/`--threads 4` to run 4 concurrent purge threads
    - `-f Prefix`/`--start-at Prefix` to skip all pages until a prefix is encountered.
    - `-n 0 1`/`--namespaces 0 1` to purge specific namespaces (here: main, Talk)
    - `-x`/`--exclude-namespaces` to invert the namespace check (i.e. purge all namespaces except for main & Talk). Requires `--namespaces`.
    - List wikis after all parameters, as positional arguments. Format is `domain:language`, e.g. `confluence:en`

--- purge_wiki.py
+++ purge_wiki.py
@@ -1,38 +1,133 @@
+import argparse
 import time
 
+import requests
+import urllib.parse
+from concurrent.futures import ThreadPoolExecutor
+from requests.exceptions import HTTPError
+from mwcleric import AuthCredentials, WikiggClient
 from mwcleric.errors import RetriedLoginAndStillFailed
-from mwcleric.wikigg_client import WikiggClient
-from mwcleric.auth_credentials import AuthCredentials
 
-credentials = AuthCredentials(user_file="me")
 
-wikis = ['gg']
+WIKIS = ['gg:en']
+NAMESPACES = []
+EXCLUDE_NAMESPACES = False
+DO_NULL_EDITS = False
+FROM_PAGE = ''
+THREAD_COUNT = 1
+DO_WARMUP_CACHE_MAYBE = False
 
-for wiki in wikis:
-    limit = -1
-    startat_page = None
-    # startat_page = 'Zuchtzentrum'
-    site = WikiggClient(wiki, credentials=credentials)  # Set wiki
-    # this_template = site.client.pages['Template:Infobox Player']  # Set template
-    # pages = this_template.embeddedin()
 
-    # pages = site.client.categories['Pages with script errors']
+class PurgeBot:
+    namespaces: list[int]
+    do_null: bool
+    start_at: str
+    thread_count: int = 1
+    warmup: bool
 
-    passed_startat = False if startat_page else True
-    lmt = 0
-    for ns in site.client.namespaces:
+    def __init__(self,
+                 target_name,
+                 target_lang,
+                 namespaces,
+                 exclude_namespaces,
+                 do_null,
+                 thread_count,
+                 start_at,
+                 warmup):
+        self.passed_startat = False
+        credentials = AuthCredentials(user_file="me")
+        self.target_name = target_name
+        self.target_lang = target_lang
+        self.namespaces = namespaces
+        self.do_null = do_null
+        self.start_at = start_at
+        self.warmup = warmup
+
+        self.target = WikiggClient(target_name, credentials=credentials, lang=target_lang)
+
+        if not namespaces:
+            self.namespaces = self.target.client.namespaces
+
+        if namespaces and exclude_namespaces:
+            self.namespaces = [item for item in self.target.client.namespaces if item not in namespaces]
+
+        if thread_count > 1:
+            self.thread_count = thread_count
+
+    def run(self):
+        for ns in self.namespaces:
+            self.process_namespace(ns)
+
+    def process_namespace(self, ns, pool: ThreadPoolExecutor|None=None):
         if ns < 0:
-            continue
-        for page in site.client.allpages(namespace=ns):
-            if lmt == limit:
-                break
-            if startat_page and page.name == startat_page:
-                passed_startat = True
-            if not passed_startat:
-                continue
-            print('Purging page %s...' % page.name)
+            print(f'Namespace cannot be edited: {ns}')
+            return
+
+        if ns not in self.target.client.namespaces:
+            print(f'Remote wiki does not have namespace #{ns}')
+            return
+
+        if self.thread_count > 1 and not pool:
+            with ThreadPoolExecutor(max_workers=self.thread_count) as pool:
+                return self.process_namespace(ns, pool)
+
+        print(f'Purging namespace #{ns}')
+        for page in self.target.client.allpages(namespace=ns):
+            if self.start_at:
+                if page.name.startswith(self.start_at):
+                    self.start_at = None
+                else:
+                    continue
+
+            if pool:
+                pool.submit(self.process_page, page)
+            else:
+                self.process_page(page)
+
+    def process_page(self, page):
+        while True:
             try:
-                site.touch(page)
-                site.purge(page)
-            except RetriedLoginAndStillFailed:
-                continue
+                print(page.name)
+                if self.do_null:
+                    self.target.touch(page)
+                self.target.purge(page)
+
+                if self.warmup:
+                    encoded_name = urllib.parse.quote(page.name)
+                    article_url = f'https://{self.target.url}{self.target.path}wiki/{encoded_name}'
+                    response = requests.get(article_url)
+                    if response.status_code >= 400:
+                        print('HTML cache warm-up failed:', article_url)
+
+                break
+            except (HTTPError, RetriedLoginAndStillFailed) as e:
+                print(e)
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--namespaces', type=int, nargs='*', default=NAMESPACES)
+    parser.add_argument('-x', '--exclude-namespaces', action='store_true', default=EXCLUDE_NAMESPACES)
+    parser.add_argument('-e', '--nulledit', action='store_true', default=DO_NULL_EDITS)
+    parser.add_argument('-t', '--threads', type=int, default=THREAD_COUNT)
+    parser.add_argument('-f', '--start-at', type=str, default=FROM_PAGE)
+    parser.add_argument('-w', '--warmup', action='store_true', default=DO_WARMUP_CACHE_MAYBE)
+    parser.add_argument('wikis', nargs='*', default=WIKIS)
+    args = parser.parse_args()
+
+    common_bot_args = dict(
+        do_null=args.nulledit,
+        namespaces=args.namespaces,
+        exclude_namespaces=args.exclude_namespaces,
+        thread_count=args.threads,
+        start_at=args.start_at,
+        warmup=args.warmup,
+    )
+
+    for wiki in args.wikis:
+        name, lang = wiki, None
+        if ':' in wiki:
+            name, lang = wiki.split(':')
+        PurgeBot(name, lang, **common_bot_args).run()
+
