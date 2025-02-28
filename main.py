4a5453c8cf64c8c4a2fb4c1613d88b72bc7ab1e4
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2025-02-28 02:57:53 -0600

    Change mainpage to {{SITENAME}}

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from mwcleric import WikiggClient
 from mwclient.page import Page
 
 
-WIKIS = ['gg:en']
-IS_IMPORT = False  # don't overwrite & don't make mainspace pages
+WIKIS = ['test:en']
+IS_IMPORT = True  # don't overwrite & don't make mainspace pages
 SKIP_CSS = False
 START_AT_PAGE = None
 START_AT_NAMESPACE = 0
@@ -57,24 +57,26 @@ class Loadout:
             print(doc_page_name)
             page, docpage = doc_page_name.split('/')
             self.docpage = '/' + docpage
-        
 
     def run(self):
         self.copy()
 
     def copy(self):
-        for ns in self.loadout.client.namespaces:
-            print(f"Starting namespace {ns}")
-            if ns <= self.startat_namespace - 1:  # ns 4 is Project ns
-                continue
-            if ns == 0:
-                continue
-            self.copy_namespace(ns)
+        # for ns in self.loadout.client.namespaces:
+        #     print(f"Starting namespace {ns}")
+        #     if ns <= self.startat_namespace - 1:  # ns 4 is Project ns
+        #         continue
+        #     if ns == 0:
+        #         continue
+        #     self.copy_namespace(ns)
         if not self.is_import:
             self.copy_namespace(0)
-        else:
-            self.redirect_mainpage()
-            self.add_user_migration_notes()
+            return
+        # for imported wikis
+        self.move_old_mainpage()
+        self.redirect_mainpage()
+        self.add_user_migration_notes()
+        self.copy_namespace(0)
 
     def copy_namespace(self, ns: int):
         for orig_page in self.loadout.client.allpages(namespace=ns):
@@ -109,13 +111,15 @@ class Loadout:
         if not self.is_import:
             # if it's not an import we always do the save
             # except at page MediaWiki copyright, then we don't want to overwrite
-            # if new_title != 'MediaWiki:Copyright':
             if new_title != 'MediaWiki:Copyright' or not target_page.exists:
                 do_save = True
         elif new_title in ['MediaWiki:Common.css', 'MediaWiki:Vector.css']:
             if not self.skip_css:
                 do_save = True
         elif not target_page.exists and new_title != 'MediaWiki:Copyright':
+            do_save = True
+        elif ns == 0:
+            # overwrite the main page + imp pages
             do_save = True
         if do_save:
             self.save(target_page, orig_page)
@@ -134,6 +138,17 @@ class Loadout:
                 self.target.protect(target_page, protections=protections)
             except Exception as e:
                 pass
+
+    def move_old_mainpage(self):
+        orig_page_name = self.target.client.site['mainpage']
+        new_page_name = self.target.client.site['sitename']
+        if orig_page_name == new_page_name:
+            return
+        if (orig_page := self.target.client.pages[orig_page_name]).exists:
+            if (wrong_orig_page := self.target.client.pages[new_page_name]).exists:
+                wrong_orig_page.delete("Making way for existing main page")
+            self.target.move(orig_page, new_page_name)
+        self.target.client.pages['MediaWiki:mainpage'].save('{{SITENAME}}', 'renaming main page to SITENAME')
 
     def redirect_mainpage(self):
         mainpage = self.target.client.pages['Main Page']
